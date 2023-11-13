b1065c297e350ec669691b4a6f15c02ba85c2621
Tyler <tylergross28@gmail.com>
2023-11-13 17:10:10 -0500

    base stuff reading from dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+#include <execution>
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+fs::path test_images_path =
+    "/Users/tgross/Documents/GitHub/png-benchmarks/images";
 
 int main() {
-  std::cout << "Hello!" << std::endl;
+  auto images_to_compress = std::vector<fs::path>{};
+  for (const auto& entry : fs::directory_iterator(test_images_path)) {
+    images_to_compress.push_back(entry.path());
+  }
+
+  for (const auto& path : images_to_compress) {
+    std::cout << path.string() << std::endl;
+  }
 }
