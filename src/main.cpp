f5ab12b7da57d42ba1325a58f55b018d307acd89
Tyler <tylergross28@gmail.com>
2023-11-13 17:46:17 -0500

    delete crap

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -13,10 +14,12 @@ fs::path test_images_path =
 int main() {
   auto images_to_compress = std::vector<fs::path>{};
   for (const auto& entry : fs::directory_iterator(test_images_path)) {
-    images_to_compress.push_back(entry.path());
+    images_to_compress.push_back(entry);
   }
 
-  for (const auto& path : images_to_compress) {
-    std::cout << path.string() << std::endl;
-  }
+  std::for_each(std::execution::par, std::begin(images_to_compress), std::end(images_to_compress), [&] (fs::path path) {
+    if (fs::is_regular_file(path)) {
+      std::ifstream png_file{path, std::ios::binary};
+    }
+  });
 }
